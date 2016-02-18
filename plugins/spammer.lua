cb0cc3e7fe77803faabfdbfdd5e876ba09ccc5e1
githubmereza <github09355332076@gmail.com>
2016-02-18 11:58:51 +0330

    Create spammer.lua

---
+++ plugins/spammer.lua
@@ -0,0 +1,18 @@
+local function run(msg, matches)
+
+  local receiver = get_receiver(msg)
+    if matches[1] == "spam" and is_member,admin(msg) then
+    local num = matches[2]
+     local text = matches[3]
+        for i=1,num do
+            send_large_msg(receiver, text)
+        end
+  end
+end
+ 
+return {
+  patterns = {
+  "^اسپم (%d+) (.*)$",
+  },
+  run = run,
+}
