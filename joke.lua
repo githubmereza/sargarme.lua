4faccc4a320c4fd93bf2c94ec7c48fd6124150b9
githubmereza <github09355332076@gmail.com>
2016-02-18 10:55:19 +0330

    Create joke.lua

---
+++ joke.lua
@@ -0,0 +1,3 @@
+local database= 'http://umbrella.shayan-soft.ir/txt/' local function run(msg) local res = http.request(database.."joke.db") local joke = res:split(",") return joke[math.random(#joke)] end
+
+return { description = "500 Persian Joke", usage= "!joke : send random joke", patterns = {"^جوک$"}, run = run }
