959f260304c40eb17f4b0c2460ac3a8880084a76
githubmereza <github09355332076@gmail.com>
2016-02-18 10:52:28 +0330

    Create jomlack.lua

---
+++ plugins/jomlack.lua
@@ -0,0 +1,3 @@
+local database= 'http://umbrella.shayan-soft.ir/txt/' local function run(msg) local res = http.request(database.."jomlak.db") local jomlak = res:split(",") return jomlak[math.random(#jomlak)] end
+
+return { description = "JomlakSender", usage= "!jomlak: send random texts", patterns = {"^جملک$"}, run = run }
