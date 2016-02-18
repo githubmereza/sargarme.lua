041f7cc7121ee41f77490fe799b0a7ba58c352b8
githubmereza <github09355332076@gmail.com>
2016-02-18 10:53:50 +0330

    Create danestani.lua

---
+++ plugins/danestani.lua
@@ -0,0 +1,3 @@
+local database= 'http://umbrella.shayan-soft.ir/txt/' local function run(msg) local res = http.request(database.."danestani.db") local danestani = res:split(",") return danestani[math.random(#danestani)] end
+
+return { description = "Knowing Book", usage= "!danestani : send knowing texts", patterns = {"^دانستنیها$"}, run = run } S
